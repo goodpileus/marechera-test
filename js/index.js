0b45d187f386f5235385a48e12d696693b1d94ca
aidan <aidankquinlan@gmail.com>
2019-07-29 21:18:38 -0400

    flickity implementation

--- js/index.js
+++ js/index.js
@@ -1,36 +1,57 @@
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-  var slideIndex = 1;
-  showDivs(slideIndex);
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+//   // document.querySelector('.about').style.display = "flex";
+//   document.querySelector('html').style.overflowY = "scroll";
+//   // document.querySelector('.feature').style.height = "97vh";
+//   document.querySelector('.down-arrow').style.bottom = "2rem";
+// }
 
-  function plusDivs(n) {
-    showDivs(slideIndex += n);
-    document.querySelector('.about').style.display = "flex";
-  }
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("slides");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length} ;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   x[slideIndex-1].style.display = "block";
+// }
 
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("slides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
-  }
+// var ul = document.querySelector('ul');
+// for (var i = ul.children.length; i >= 0; i--) {
+//     ul.appendChild(ul.children[Math.random() * i | 0]);
+// }
+//
+// document.onscroll = function(){
+//   document.querySelector('.down-arrow').style.opacity = "0";
+// };
 
-  var ul = document.querySelector('ul');
-  for (var i = ul.children.length; i >= 0; i--) {
-      ul.appendChild(ul.children[Math.random() * i | 0]);
-  }
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  wrapAround: true,
+  prevNextButtons: false,
+  draggable: true,
+  pageDots: false,
+  imagesLoaded: true,
+  freeScroll: true,
+  lazyLoad: true,
+  dragThreshold: 1,
+  autoPlay: 1500,
+  freeScrollFriction: 0.03
 });
+
+// carousel static click to advance slide
+    flkty.on('staticClick', function(event, pointer, cellElement, cellIndex) {
+      if (!cellElement) {
+        return;
+      }
+
+      if (cellIndex == flkty.selectedIndex) {
+        flkty.next();
+      } else {
+        flkty.select(cellIndex);
+      }
+    });
