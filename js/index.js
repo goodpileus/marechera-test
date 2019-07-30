718740cc9d623618f088c584892783b04d1dfb8c
aidan <aidankquinlan@gmail.com>
2019-07-30 00:18:45 -0400

    changing text

--- js/index.js
+++ js/index.js
@@ -1,31 +1,38 @@
+var slideIndex = 1;
+showDivs(slideIndex);
 
-  var slideIndex = 1;
-  showDivs(slideIndex);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+  // document.querySelector('.about').style.display = "flex";
+  // document.querySelector('.feature').style.height = "97vh";
+  document.querySelector('.down-arrow').style.bottom = "2rem";
+}
 
-  function plusDivs(n) {
-    showDivs(slideIndex += n);
-    // document.querySelector('.about').style.display = "flex";
-    document.querySelector('html').style.overflowY = "scroll";
-    // document.querySelector('.feature').style.height = "97vh";
-    document.querySelector('.down-arrow').style.bottom = "2rem";
+function initScroll() {
+  document.querySelector('html').style.overflowY = "scroll";
+}
+
+// function toggleCaption() {
+//   var caption = document.querySelector(".caption");
+//   caption.classList.toggle("show");
+// }
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  x[slideIndex-1].style.display = "flex";
+}
 
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("slide");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.opacity = "0";
-    }
-    x[slideIndex-1].style.opacity = "1";
-  }
+var ul = document.querySelector('ul');
+for (var i = ul.children.length; i >= 0; i--) {
+    ul.appendChild(ul.children[Math.random() * i | 0]);
+}
 
-  var ul = document.querySelector('ul');
-  for (var i = ul.children.length; i >= 0; i--) {
-      ul.appendChild(ul.children[Math.random() * i | 0]);
-  }
-
-  document.onscroll = function(){
-    document.querySelector('.down-arrow').style.opacity = "0";
-  };
+document.onscroll = function(){
+  document.querySelector('.down-arrow').style.opacity = "0";
+};
