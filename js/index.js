f69a0d964308b8586cfc238e200fcd76539ff0a5
aidan <aidankquinlan@gmail.com>
2019-07-31 01:24:30 -0400

    cleaned up

--- js/index.js
+++ js/index.js
@@ -19,9 +19,11 @@ function showDivs(n) {
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    // x[i].style.display = "none";
+    x[i].style.opacity = "0";
   }
-  x[slideIndex-1].style.display = "flex";
+  // x[slideIndex-1].style.display = "flex";
+  x[slideIndex-1].style.opacity = "1";
 }
 
 var ul = document.querySelector('ul');
