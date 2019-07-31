ab2ff4093e2d45126652033fa82e1ffd5ce180b3
aidan <aidankquinlan@gmail.com>
2019-07-31 17:55:53 -0400

    caption implementation + fades

--- js/index.js
+++ js/index.js
@@ -21,9 +21,12 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
     // x[i].style.display = "none";
     x[i].style.opacity = "0";
+    x[i].style.zIndex = "0";
+
   }
   // x[slideIndex-1].style.display = "flex";
   x[slideIndex-1].style.opacity = "1";
+  x[slideIndex-1].style.zIndex = "999";
 }
 
 // var ul = document.querySelector('ul');
@@ -45,10 +48,25 @@ function showAbout() {
   // document.querySelector('html').style.overflowY = "scroll";
 }
 
-var caption = document.querySelector('.caption');
+// var caption = document.querySelector('.caption');
 
-function toggleCaption() {
-  caption.classList.toggle("caption-show");
+// function toggleCaption() {
+//   caption.classList.toggle("caption-show");
+// }
+
+var captionBtn = document.getElementsByClassName("caption-btn");
+var i;
+
+for (i = 0; i < captionBtn.length; i++) {
+  captionBtn[i].addEventListener("click", function() {
+
+    // resizes window to fix flickity height
+    // window.dispatchEvent(new Event('resize'));
+
+    // this.classList.toggle("caption-show");
+    var caption = this.nextElementSibling;
+    caption.classList.toggle("caption-show");
+  });
 }
 
 fitText(document.getElementById('fittext'), .92)
