cdaa2842db2706480e0ccd16ce5c8b59c26c56d7
aidan <aidankquinlan@gmail.com>
2019-08-01 01:06:03 -0400

    annotated, cleaned

--- js/index.js
+++ js/index.js
@@ -1,17 +1,12 @@
+
+// ADVANCE SLIDES
+// ---------------------------------------------
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
-  // document.querySelector('.about').style.display = "flex";
-  // document.querySelector('.feature').style.height = "97vh";
-  // document.querySelector('.down-arrow').style.bottom = "2rem";
 }
-
-// function toggleCaption() {
-//   var caption = document.querySelector(".caption");
-//   caption.classList.toggle("show");
-// }
 
 function showDivs(n) {
   var i;
@@ -19,58 +14,61 @@ function showDivs(n) {
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
-    // x[i].style.display = "none";
     x[i].style.opacity = "0";
     x[i].style.zIndex = "0";
-
   }
-  // x[slideIndex-1].style.display = "flex";
   x[slideIndex-1].style.opacity = "1";
   x[slideIndex-1].style.zIndex = "999";
 }
 
+
+
+// RANDOM LIST
+// ---------------------------------------------
 var random = document.querySelector('.random');
 for (var i = random.children.length; i >= 0; i--) {
     random.appendChild(random.children[Math.random() * i | 0]);
 }
 
-document.onscroll = function(){
-  document.querySelector('.down-arrow').style.opacity = "0";
-};
 
+
+// ABOUT OPEN/CLOSE
+// ---------------------------------------------
 var about = document.querySelector('.about');
 var aboutBtn = document.querySelector('.about-btn');
+
 function showAbout() {
-  // about.style.transform = "translateY(-100%)";
   about.classList.toggle("show");
   aboutBtn.classList.toggle("close");
-
-  // document.querySelector('html').style.overflowY = "scroll";
 }
 
-// var caption = document.querySelector('.caption');
 
-// function toggleCaption() {
-//   caption.classList.toggle("caption-show");
-// }
 
+// CAPTION BUTTON (SHOW/HIDE)
+// ---------------------------------------------
 var captionBtn = document.getElementsByClassName("caption-btn");
 var i;
 
 for (i = 0; i < captionBtn.length; i++) {
   captionBtn[i].addEventListener("click", function() {
 
-    // resizes window to fix flickity height
-    // window.dispatchEvent(new Event('resize'));
-
-    // this.classList.toggle("caption-show");
+    // show/hide caption on mouse click
     var caption = this.nextElementSibling;
     caption.classList.toggle("caption-show");
   });
 }
 
+
+
+// UNUSED FIT TEXT
+// ---------------------------------------------
 // fitText(document.getElementById('fittext'), .92)
 
+
+
+
+// UNUSED SCROLL
+// ---------------------------------------------
 // const button = document.querySelector('.slide-wrap');
 // var width = document.documentElement.clientWidth;
 // var container = document.querySelector('.slide-wrap')
